2683526708dd6c484997a0d6b03dd07e400e0399
Truphile <salidmonreal@gmail.com>
2026-04-14 13:02:42 +0100

    api commit

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from routes.classify import router
 app = FastAPI()
 
 
-app.add_middleware(CORSMiddleware, allow_origins=["*"],
-                   allow_headers=["*"],
-                   allow_methods=["*"],
+app.add_middleware(CORSMiddleware, allow_origins=['*'],
+                   allow_headers=['*'],
+                   allow_methods=['*'],
                    allow_credentials=True)
 
 
